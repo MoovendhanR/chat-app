cb9c201964d2c3651ff39c3b1aaece2ce3a1b392
MoovendhanR <moovendhanr1011@gmail.com>
2024-03-16 16:46:31 +0530

    authorization part completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signupUser = async (req, res) => {
   try {
@@ -13,6 +15,8 @@ export const signupUser = async (req, res) => {
     }
 
     //hash password here..
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     // avatar placeholder
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -20,29 +24,60 @@ export const signupUser = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
-      
     });
-    await newUser.save();
-        res.status(201).json({
-            _id:newUser._id,
-            fullName:newUser.fullName,
-            username:newUser.username,
-            profilePic:newUser.profilePic
-        });
-        // send({newUser:newUser})
+    if (newUser) {
+        // generating token ..
+         generateTokenAndSetCookie(newUser._id,res)
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+    // send({newUser:newUser})
   } catch (error) {
-     console.log("error in signup controller",error.message);
-     res.status(500).json({error:"Internal Server Error"})
+    console.log("error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 export const loginUser = async (req, res) => {
   try {
-  } catch (err) {}
+     const {username,password} = req.body;
+     const user = await User.findOne({username});
+     const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+
+     if(!user ||!isPasswordCorrect){
+      return res.status(400).json({error:"Invalid username or password"});
+     }
+
+     generateTokenAndSetCookie(user._id,res);
+
+     res.status(200).json({
+      _id:user._id,
+      fullName:user.fullName,
+      username:user.username,
+      profilePic:user.profilePic,
+     });
+
+  } catch (error) {
+    console.log("error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-export const logout = (req, res) => {
-  res.send("logout user");
+export const logout = async(req, res) => {
+  try {
+     res.cookie("jwt","",{maxAge:0});
+     res.status(200).json({message:"Logged out successfully"})
+  } catch (error) {
+    console.log("error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+  res.cookie("twt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, //ms
+    httpOnly: true, //prevent xss attacks cross-site scripting attack
+    sameSite: "strict", //csrf attack cross-site request forgery attacks
+    secure:process.env.NODE_ENV !== "development"
+  });
+};
+
+export default generateTokenAndSetCookie;
