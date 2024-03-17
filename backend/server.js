c0508a68b785fdc80b77d1c1014a84404bf2ff14
MoovendhanR <moovendhanr1011@gmail.com>
2024-03-17 12:50:55 +0530

    send message part done

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from './routes/messages.routes.js'
 import ConnectMongoDB from "./config/db.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json());//to parse the incoming requests with json payloads(from req.body)
+app.use(cookieParser())
 
 app.get("/",(req,res)=>{
     // root route
     res.send("hello world!!")
 })
 app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+
+
 
 app.listen(PORT,()=>{
     ConnectMongoDB()
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
-  res.cookie("twt", token, {
+  res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, //ms
     httpOnly: true, //prevent xss attacks cross-site scripting attack
     sameSite: "strict", //csrf attack cross-site request forgery attacks
