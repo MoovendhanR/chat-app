16483b2ac0728e0b1e866a1112a6e7659867a165
MoovendhanR <moovendhanr1011@gmail.com>
2024-03-17 16:21:42 +0530

    backend part completed

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());//to parse the incoming requests with json payloads(from req.body)
 app.use(cookieParser())
 
-app.get("/",(req,res)=>{
-    // root route
-    res.send("hello world!!")
-})
+// app.get("/",(req,res)=>{
+//     // root route
+//     res.send("hello world!!")
+// })
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
